25bfaf183824dbb505e64e58991e29f7e1833242
Kulikov17 <dakulikov2001@mail.ru>
2024-03-26 20:09:33 +0300

    done task

--- backend/src/ml/models.py
+++ backend/src/ml/models.py
@@ -12,3 +12,26 @@ def load_model(model_pth, num_classes=196):
     model.load_state_dict(checkpoint['model_state_dict'])
 
     return model
+
+
+def build_model(pretrained=True, fine_tune=True, num_classes=196):
+    if pretrained:
+        print('[INFO]: Loading pre-trained weights')
+    else:
+        print('[INFO]: Not loading pre-trained weights')
+
+    model = models.efficientnet_v2_s(pretrained=pretrained)
+
+    if fine_tune:
+        print('[INFO]: Fine-tuning all layers...')
+        for params in model.parameters():
+            params.requires_grad = True
+    elif not fine_tune:
+        print('[INFO]: Freezing hidden layers...')
+        for params in model.parameters():
+            params.requires_grad = False
+
+    # Change the final classification head.
+    model.classifier[1] = nn.Linear(in_features=1280, out_features=num_classes)
+
+    return model
---
+++ backend/src/routes/model.py
@@ -0,0 +1,69 @@
+import io
+from fastapi import APIRouter, HTTPException, UploadFile
+from typing import List
+from PIL import Image
+from src.db.db_connect import db_conn
+from src.db.db_execute_query import db_execute_query
+from src.celery.tasks import train_model_task
+from src.dto.train import TrainStatusDto
+from src.dto.prediction import PredictionDto
+from src.ml.models import load_model
+from src.ml.predict import predict
+from celery import uuid
+
+
+router = APIRouter(prefix='/model')
+
+
+@router.get('/train_statuses')
+async def train_statuses_model() -> List[TrainStatusDto]:
+    try:
+        conn = db_conn()
+        conn.autocommit = True
+
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM celery')
+
+        columns = [col[0] for col in cursor.description]
+        statuses = [dict(zip(columns, row)) for row in cursor.fetchall()]
+
+        cursor.close()
+        conn.close()
+
+        return statuses
+    except:
+        raise HTTPException(status_code=500, detail="Unknown Error")
+
+
+@router.get('/train')
+async def train_model(chat_id: str):
+    try:
+        dir_path = './data'
+        task_id = uuid()
+
+        query = """
+            INSERT INTO celery(task_id, status)
+            VALUES(%s, %s)
+        """
+
+        db_execute_query(query, (task_id, 'PENDING'))
+
+        train_model_task.apply_async(args=[dir_path, chat_id], task_id=task_id)
+
+        return None
+    except:
+        raise HTTPException(status_code=500, detail="Unknown Error")
+
+
+@router.post('/predict')
+async def predict_product_category(img_file: UploadFile) -> PredictionDto:
+    try:
+        contents = await img_file.read()
+        img = Image.open(io.BytesIO(contents)).convert('RGB')
+
+        model = load_model('./data/model.pth', 2)
+        result = predict(model, img)
+
+        return {'result': result}
+    except:
+        raise HTTPException(status_code=500, detail="Unknown Error")

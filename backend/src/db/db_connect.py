5a589e25844625903f5419d565eec67bea131289
Kulikov17 <dakulikov2001@mail.ru>
2024-04-06 16:14:27 +0300

    some changes

--- backend/src/db/db_connect.py
+++ backend/src/db/db_connect.py
@@ -4,7 +4,7 @@ import psycopg2
 
 def db_conn() -> psycopg2.extensions.connection:
     return psycopg2.connect(
-        host="postgres_container",
+        host="postgres",
         port=5432,
         dbname=os.environ["POSTGRES_DB"],
         user=os.environ["POSTGRES_USER"],
